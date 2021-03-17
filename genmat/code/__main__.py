45315a4ad66f0f59b94a9a70dd424988461b9ea5
Dalton Walker <dalton_walker@icloud.com>
2021-03-16 23:36:47 -0400

    EOD 20210316

--- genmat/code/__main__.py
+++ genmat/code/__main__.py
@@ -6,10 +6,11 @@ import joblib
 import time
 from drain3.drain import Drain
 from datetime import timedelta
+from collections import namedtuple
 from genmat.database_methods import create_connection
 
 
-def cluster_visualizer(df: pd.DataFrame, tf: timedelta):
+def genmat(df: pd.DataFrame, tf: timedelta):
     """
     Each cluster will
     :param
@@ -29,9 +30,9 @@ def cluster_visualizer(df: pd.DataFrame, tf: timedelta):
     df['timestamp'] = pd.to_datetime(df['timestamp'], format=fmt)
     labels = set(df['label'].unique().tolist())
 
-    all_container_names = set(df['container_name'].unique().tolist())
+    all_containers = set(df['container_name'].unique().tolist())
     forbidden_containers = {'solr'}
-    considered_container_names = all_container_names - forbidden_containers
+    considered_containers = all_containers - forbidden_containers
 
     # pd.DataFrame with True in the index where a label has changed and false otherwise
     transitions = df['label'] != df['label'].shift(axis=0)
@@ -48,7 +49,7 @@ def cluster_visualizer(df: pd.DataFrame, tf: timedelta):
     # the goal here is to make a drain model for each container that has clusters for all observations
     # from all collections so that we may see which receive messages during specific collection runs
 
-    dd = {container_name: Drain() for container_name in considered_container_names}
+    dd = {container: Drain() for container in considered_containers}
 
     for log in df.itertuples():
         if log.container_name not in forbidden_containers:
@@ -60,8 +61,13 @@ def cluster_visualizer(df: pd.DataFrame, tf: timedelta):
     dfs = {idx: df.iloc[ce[idx] + 1:ce[idx + 1] + 1, :].sort_values(by='timestamp') for idx in range(n_collections - 2)}
 
     # build the cluster count dictionary
-    cd = {idx: {label: {container_name: np.array for container_name in considered_container_names} for label in labels}
+    had = {idx: {label: {container: np.array for container in considered_containers} for label in labels}
+           for idx in dfs.keys()}
+
+    hd = {idx: {label: {container: {} for container in considered_containers} for label in labels}
           for idx in dfs.keys()}
+
+    Tally = namedtuple('Tally', ['idx', 'count'])
 
     for idx in dfs.keys():
         di = dfs[idx]
@@ -70,39 +76,47 @@ def cluster_visualizer(df: pd.DataFrame, tf: timedelta):
             start_time = dl['timestamp'].iloc[0]
             end_time = dl['timestamp'].iloc[-1]
             n_intervals = math.ceil((end_time - start_time) / tf)
-            for container_name in considered_container_names:
-                logs = di[(di['label'] == label) & (di['container_name'] == container_name)]
-                drain_model = dd[container_name]
+            if n_intervals < 1:
+                print('number of intervals is less than one!')
+                break
+            for container in considered_containers:
+                logs = dl[dl['container_name'] == container]
+                drain_model = dd[container]
                 counter_array = np.zeros((len(drain_model.clusters), n_intervals))
                 for log in logs.itertuples():
                     time_stamp = log.timestamp
                     # instead of iterating over each time period we can calculate which period the log arrived
                     col = math.floor((time_stamp - start_time) / tf)
-                    match_cluster, update_type = drain_model.add_log_message(log.log)
-                    counter_array[match_cluster.cluster_id - 1, col] += 1
-                cd[idx][label][container_name] = counter_array
+                    cluster, update_type = drain_model.add_log_message(log.log)
+                    if update_type == "none":
+                        counter_array[cluster.cluster_id - 1, col] += 1
+                    if cluster not in hd[idx][label][container].keys():
+                        hd[idx][label][container][cluster] = [cluster.cluster_id, 0]
+                    hd[idx][label][container][cluster].count += 1
+                had[idx][label][container] = counter_array
 
-    return labels, considered_container_names, dfs.keys(), cd, dd
+    return labels, considered_containers, dfs.keys(), had, hd, dd
 
 
 def main():
-    db_path = '/code/database/elastic_logs.db'
-    db_conn = create_connection(db_path)
+    db_path = 'database/tanner_logs.db'
+    db_conn = create_connection(os.environ["DB_PATH"])
 
     # make dataframe and sort by timestamp
     sql_query = 'SELECT * FROM logs'
     df = pd.read_sql_query(sql_query, db_conn)
-    tf = timedelta(seconds=int(os.getenv("TIME_FRAME")))
+    tf = timedelta(seconds=int(os.environ["TIME_FRAME"])*60)
 
     print("Starting matrix creation...")
-    labels, containers, collections, cd, dd = cluster_visualizer(df, tf)
+    labels, containers, collections, had, hd, dd = genmat(df, tf)
     print("... matrix creation complete!")
 
     print("Saving results...")
     joblib.dump(labels, "/code/results/labels.joblib")
     joblib.dump(containers, "/code/results/containers.joblib")
     joblib.dump(set(collections), "/code/results/collections.joblib")
-    joblib.dump(cd, "/code/results/matrices_dict.joblib")
+    joblib.dump(had, "/code/results/matrices_dict.joblib")
+    joblib.dump(hd, "/code/results/hits_dict.joblib")
     joblib.dump(dd, "/code/results/drain_dict.joblib")
     print("... saving complete!")
 
--- genmat/code/database_methods.py
+++ genmat/code/database_methods.py
@@ -1,16 +1,16 @@
 import sqlite3 as sql
 
 
-def create_connection(db_file: str) -> sql.Connection:
+def create_connection(path: str) -> sql.Connection:
     """
     Creates a database connection
-    :param db_file: str
+    :param path: str
         path to database object
     :return sql.Connection
         a connection to the database
     """
     try:
-        conn = sql.connect(db_file)
+        conn = sql.connect(path)
         print('connection successful')
         return conn
     except sql.Error as e:
