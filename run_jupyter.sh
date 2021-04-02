020e967d66e0babbe2bfc549303635914e48976b
Andrew Romans <andrew.romans@us.af.mil>
2021-04-02 16:07:18 -0400

    Cleaned up run_jupyter.sh

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-sudo docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v /Users/dalton/Documents/Work/ClusterAnalysisTool/results:/home/jovyan/results -v /Users/dalton/Documents/Work/ClusterAnalysisTool/training:/home/jovyan/work jupyter/datascience-notebook:latest
-
+sudo docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes \
+-v /home/aromans/Dev/log-analyzer/results:/home/jovyan/results \
+-v /home/aromans/Dev/log-analyzer/JupyterNotebooks:/home/jovyan/work \
+-v /home/aromans/Dev/log-analyzer/database:/home/jovyan/database \
+jupyter/datascience-notebook:latest
