bcdaea3be0f9d269db62af3a62bff9377af27e20
Samuel Dalton walker <dalton_walker@icloud.com>
2021-11-04 21:43:27 -0400

    Complete first stage of reorganization

--- lab/jupyter/jupyterextensions.sh
+++
@@ -1,5 +0,0 @@
-# install jupyterlab extensions
-jupyter labextension install --no-build @krassowski/jupyterlab-lsp
-jupyter labextension install --no-build @jupyterlab/debugger
-jupyter labextension install --no-build @wallneradam/output_auto_scroll
-jupyter lab build --dev-build=False --minimize=False
