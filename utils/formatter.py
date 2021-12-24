d990c92348ac5d97d6dcaf215a4b2aa443a16e75
Samuel Dalton walker <dalton_walker@icloud.com>
2021-12-23 23:23:07 -0500

    adds docs

--- utils/formatter.py
+++ utils/formatter.py
@@ -12,9 +12,9 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def add_pip_pkgs(container):
-    logger.info(f'Executing container cmd on {container}...')
-    with DockerRunner(container) as container:
+def add_pip_pkgs(container_name):    
+    logger.info(f'Executing container cmd on {container_name}...')
+    with DockerRunner(container_name) as container:
         content = DockerRunner.run_simple_command(container, cmd="pip list", print_result=False)
     logger.info('... complete')
 
@@ -28,10 +28,18 @@ def add_pip_pkgs(container):
         lines[i] = "| " + lines[i] + " |"
     logger.info('... complete')
 
-    # write_content = '\n'.join(lines)
-    # with open('pip_table.txt', 'w') as w:
-    #     w.write(write_content)
+    logger.info('writing contents to document...')
+    lines.insert(0, "# Included pip Packages:")
+    lines.append('\n')
+    lines.append('---')
+    write_content = '\n'.join(lines)
+    tag = container_name.split(':')[1]
+    image = tag.split('_')[0]
+    with open(f'docs/{image}_lab.md', 'w') as w:
+        w.write(write_content)
+    logger.info('...complete')
     
+
 if __name__ == "__main__":
     container = 'samuel62/machine_learning_lab:base_cuda_11.3.1'
 
