ca501c828797e8e78ffd7c493ec21a84e1b55c96
Samuel Dalton walker <dalton_walker@icloud.com>
2021-12-24 00:19:49 -0500

    adds conda pkgs to docs

--- utils/formatter.py
+++ utils/formatter.py
@@ -12,11 +12,16 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def add_pip_pkgs(container_name):    
-    logger.info(f'Executing container cmd on {container_name}...')
+def get_installed_pkgs(container_name: str, cmd: str) -> str:
+    logger.info(f'Executing command {cmd} on {container_name}...')
     with DockerRunner(container_name) as container:
-        content = DockerRunner.run_simple_command(container, cmd="pip list", print_result=False)
+        content = DockerRunner.run_simple_command(container, cmd=cmd, print_result=False)
     logger.info('... complete')
+    return content
+
+
+def add_pip_pkgs(container_name: str) -> str:    
+    content = get_installed_pkgs(container_name, 'pip list')
 
     logger.info('Formatting pip packages into tabular format ...')
     lines = content.split('\n')
@@ -26,21 +31,56 @@ def add_pip_pkgs(container_name):
         else:
             lines[i] = re.sub(r"\s{3,}", " | ", lines[i])
         lines[i] = "| " + lines[i] + " |"
+
+    lines.insert(0, "# Included `pip` Packages:")
     logger.info('... complete')
 
-    logger.info('writing contents to document...')
-    lines.insert(0, "# Included `pip` Packages:")
-    lines.append('\n')
-    lines.append('---')
     write_content = '\n'.join(lines)
-    tag = container_name.split(':')[1]
-    image = tag.split('_')[0]
-    with open(f'docs/{image}_lab.md', 'w') as w:
-        w.write(write_content)
-    logger.info('...complete')
+    return write_content
+
+
+
+def add_conda_pkgs(container_name: str) -> str:
+    content = get_installed_pkgs(container_name, 'conda list')
+
+    logger.info('Formatting conda packages into tabular format ...')
+    lines = content.split('\n')
+    for i in range(len(lines)):
+        if i == 2:
+            # the third line contains the headers, get rid of the comments
+            headers = re.sub(r"\s{1,}", " | ", lines[i][2:])
+            lines[i] = headers
+        elif i > 2:
+            lines[i] = re.sub(r"\s{1,}", " | ", lines[i])
+        
+        lines[i] = "| " + lines[i] + " |"
+
+    # the first two line are comments, delete them
+    for _ in range(2): del lines[0]
+        
+    lines.insert(0, '# `conda` packages')
+    lines.insert(2, '| --- | --- | --- | ---|')
+    logger.info('... complete')
+
+    write_content = '\n'.join(lines)
+    return write_content
     
 
 if __name__ == "__main__":
-    container = 'samuel62/machine_learning_lab:base_cuda_11.3.1'
+    container_name = 'samuel62/machine_learning_lab:base_cuda_11.3.1'
 
-    add_pip_pkgs(container)
+    pip_content = add_pip_pkgs(container_name)
+    conda_content = add_conda_pkgs(container_name)
+
+    tag = container_name.split(':')[1]
+    image_type = tag.split('_')[0]
+    image = f'docs/{image_type}_lab.md'
+
+    logger.info('Writing content to documents ...')
+    with open(image, 'w') as w:
+        w.write(pip_content)
+        w.write('\n')
+        w.write('---')
+        w.write('\n')
+        w.write(conda_content)
+    logger.info('...complete')
